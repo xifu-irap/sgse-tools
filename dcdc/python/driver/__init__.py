8400d42a0c1e17d5a540b7d6a347a5db44ba404d
KenjiDeLaRosa <kenji.delarosa@alten.com>
2024-02-01 13:29:14 +0100

    dcdc: add check_equal function

--- dcdc/python/driver/__init__.py
+++ dcdc/python/driver/__init__.py
@@ -34,7 +34,7 @@
 #__all__ = ['driver']
 
 # deprecated to keep older scripts who import this from breaking
-from .utils_tools import Display
+from .utils_tools import Display,check_equal
 from .driver import Driver
 from .dcdc import DCDC
 
